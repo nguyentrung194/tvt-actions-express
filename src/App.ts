9230c0aa6f739f9d15d39f268394b8916dadfae2
nguyentrung194 <trungdipdo5@gmail.com>
2021-05-31 21:47:10 +0700

    update

--- src/App.ts
+++ src/App.ts
@@ -2,9 +2,7 @@ import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as express from 'express';
 import * as helmet from 'helmet';
-import * as morgan from 'morgan';
 import apiV1 from './apiV1/index';
-import * as errorHandler from './helpers/errorHandler';
 
 class App {
   public express: express.Application;
@@ -13,12 +11,10 @@ class App {
     this.express = express();
     this.setMiddlewares();
     this.setRoutes();
-    this.catchErrors();
   }
 
   private setMiddlewares(): void {
     this.express.use(cors());
-    this.express.use(morgan('dev'));
     this.express.use(bodyParser.json());
     this.express.use(bodyParser.urlencoded({ extended: false }));
     this.express.use(helmet());
@@ -28,10 +24,6 @@ class App {
     this.express.use('/v1', apiV1);
   }
 
-  private catchErrors(): void {
-    // this.express.use(errorHandler.notFound);
-    // this.express.use(errorHandler.internalServerError);
-  }
 }
 
 export default new App().express;
