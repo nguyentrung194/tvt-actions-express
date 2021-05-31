0e61046ac28c163aa6b3043fc42468d3d05f7855
nguyentrung194 <trungdipdo5@gmail.com>
2021-06-01 00:02:08 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const App_1 = require("./App");
-const PORT = "5001";
-App_1.default.listen(PORT, () => {
-    console.log(`Server is listening on ${5001}`);
+App_1.default.listen(process.env.PORT || 5001, () => {
+    console.log(`Server is listening on ${process.env.PORT || 5001}`);
 });
 //# sourceMappingURL=index.js.map
