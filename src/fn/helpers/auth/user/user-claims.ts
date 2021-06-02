52b6534ef2a93ac9461291582c4f04c810602449
nguyentrung194 <trungdipdo5@gmail.com>
2021-06-02 16:07:00 +0700

    update

--- src/fn/helpers/auth/user/user-claims.ts
+++ src/fn/helpers/auth/user/user-claims.ts
@@ -1,6 +1,6 @@
 export const userCustomClaims = (userId: string, id: string) => ({
   "https://hasura.io/jwt/claims": {
-    "x-hasura-default-role": "anonymous",
+    "x-hasura-default-role": "user",
     "x-hasura-allowed-roles": ["user"],
     "x-hasura-user-id": userId,
     "x-hasura-user-id-on-db": id,
